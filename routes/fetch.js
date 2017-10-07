62af314839324d52758150c0b25dfff302bdce51
wf-chris-li <nwe.cli@westfield.com>
2017-10-07 14:46:53 -0700

    chatModel

--- routes/fetch.js
+++ routes/fetch.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var chatModel = require("../database/chatmodel");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({message: "woot"});
+  chatModel.getAll(function(error, result) {
+    if(error) {
+        return res.status(400).send(error);
+    }
+    return res.send(result);
+  });
 });
 
 module.exports = router;
